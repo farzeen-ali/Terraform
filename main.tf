5254ac5b2f40fe5f075e6fd6b99782529fb24fd5
unknown <farzeenaliarif786@gmail.com>
2025-06-18 22:07:58 +0500

    Terraform Variables & Code Best Practices

--- main.tf
+++ main.tf
@@ -1,11 +1,35 @@
+# provider "aws" {
+#     region = "eu-north-1"
+# }
+
+# resource "aws_instance" "my_ec2" {
+#     ami = "ami-05fcfb9614772f051"
+#     instance_type = "t3.micro"
+#     tags = {
+#         Name = "MyFirstTerraformEC2"
+#     }
+# }
+
+# Variables
 provider "aws" {
-    region = "eu-north-1"
+    region = var.aws_region
+}
+
+data "aws_ami" "amazon_linux" {
+    most_recent = true
+    owners = ["amazon"]
+
+    filter {
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    }
 }
 
 resource "aws_instance" "my_ec2" {
-    ami = "ami-05fcfb9614772f051"
-    instance_type = "t3.micro"
+    ami = data.aws_ami.amazon_linux.id
+    instance_type = var.instance_type
+
     tags = {
-        Name = "MyFirstTerraformEC2"
+        Name = var.instance_name
     }
 }
