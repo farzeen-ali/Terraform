d34e4eec68c9debd97fb199f56f1a034296ef790
unknown <farzeenaliarif786@gmail.com>
2025-06-29 21:59:36 +0500

    Terraform Registry Modules

--- main.tf
+++ main.tf
@@ -121,26 +121,51 @@
 
 # Workspaces
 
+# provider "aws" {
+#     region = var.aws_region
+# }
+
+# data "aws_ami" "amazon_linux" {
+#     most_recent = true
+#     owners = ["amazon"]
+
+#     filter {
+#         name = "name"
+#         values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#     }
+# }
+
+# resource "aws_instance" "my_ec2" {
+#     ami = data.aws_ami.amazon_linux.id
+#     instance_type = var.instance_type
+
+#     tags = {
+#         Name = "EC2-${terraform.workspace}"
+#         Environment = terraform.workspace
+#     }
+# }
+
+# Terraform Registry Modules
+
 provider "aws" {
     region = var.aws_region
 }
 
-data "aws_ami" "amazon_linux" {
-    most_recent = true
-    owners = ["amazon"]
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-    }
-}
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
-resource "aws_instance" "my_ec2" {
-    ami = data.aws_ami.amazon_linux.id
-    instance_type = var.instance_type
+  azs             = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    tags = {
-        Name = "EC2-${terraform.workspace}"
-        Environment = terraform.workspace
-    }
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
